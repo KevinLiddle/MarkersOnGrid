76bd58abebe1574de0d4ae5c0e1585ab0fc41794
Kevin Liddle <kevin@8thlight.com>
2015-06-12 20:41:45 -0500

    got a play again footer going, but when i render it, the bottom row disappears...

--- MarkersOnGrid/TicTacToe/GameController.swift
+++ MarkersOnGrid/TicTacToe/GameController.swift
@@ -17,12 +17,12 @@ class GameController: UICollectionViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     collectionView!.registerClass(GridCell.self, forCellWithReuseIdentifier: "GridCell")
+    collectionView!.registerClass(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionFooter, withReuseIdentifier: "Footer")
+    collectionView!.registerClass(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionFooter, withReuseIdentifier: "Header")
+    collectionView!.registerClass(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionFooter, withReuseIdentifier: "Empty")
     collectionView!.backgroundColor = Colors.BACKGROUND
-    centerBoard(UIScreen.mainScreen().bounds.size)
-  }
-
-  override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
-    centerBoard(size)
+    collectionView!.setTranslatesAutoresizingMaskIntoConstraints(true)
+    collectionView!.autoresizingMask = UIViewAutoresizing.FlexibleTopMargin
   }
 
   // click event callback for each cell
@@ -40,9 +40,7 @@ class GameController: UICollectionViewController {
     }
 
     if rules.hasWinner() {
-      renderPlayAgainButton()
-    } else if rules.isCatsGame() {
-      // do other stuff
+      collectionView.reloadSections(NSIndexSet(index: board.dimension - 1))
     }
   }
 
@@ -66,6 +64,13 @@ class GameController: UICollectionViewController {
     return board.dimension
   }
 
+  override func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+    if indexPath.section == board.dimension - 1 && rules.isOver() {
+      return renderPlayAgainButton(collectionView, indexPath: indexPath)
+    }
+    return collectionView.dequeueReusableSupplementaryViewOfKind(UICollectionElementKindSectionFooter, withReuseIdentifier: "Empty", forIndexPath: indexPath) as! UICollectionReusableView
+  }
+
   func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
     return CGSizeMake(cellLength(), cellLength())
   }
@@ -74,38 +79,28 @@ class GameController: UICollectionViewController {
     return 0.0
   }
 
-  private func renderPlayAgainButton() {
-    var playAgain = NSBundle.mainBundle().loadNibNamed("PlayAgain", owner: self, options: nil)[0] as! PlayAgain
-    playAgain.setTranslatesAutoresizingMaskIntoConstraints(false)
-    self.view.addSubview(playAgain)
-//    self.view.addConstraints([
-//      NSLayoutConstraint(
-//        item: playAgain,
-//        attribute: NSLayoutAttribute.CenterX,
-//        relatedBy: NSLayoutRelation.Equal,
-//        toItem: UIScreen.mainScreen(),
-//        attribute: NSLayoutAttribute.CenterX,
-//        multiplier: 1.0,
-//        constant: 0.0
-//      ),
-//      NSLayoutConstraint(
-//        item: playAgain,
-//        attribute: NSLayoutAttribute.Bottom,
-//        relatedBy: NSLayoutRelation.LessThanOrEqual,
-//        toItem: collectionView,
-//        attribute: NSLayoutAttribute.Bottom,
-//        multiplier: 1.0,
-//        constant: -playAgain.bounds.height
-//      )
-//    ])
+  func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+    if section < board.dimension - 1 {
+      return CGSize.zeroSize
+    } else {
+      return CGSize(width: UIScreen.mainScreen().bounds.width, height: 100)
+    }
   }
 
-  private func centerBoard(size: CGSize) {
-    collectionView!.contentInset = UIEdgeInsetsMake(sidePadding(size.height), sidePadding(size.width), 0, 0)
-  }
+  private func renderPlayAgainButton(collectionView: UICollectionView, indexPath: NSIndexPath) -> UICollectionReusableView {
+    let footer = collectionView.dequeueReusableSupplementaryViewOfKind(
+      UICollectionElementKindSectionFooter,
+      withReuseIdentifier: "Footer",
+      forIndexPath: indexPath
+    ) as! UICollectionReusableView
 
-  private func sidePadding(sideDimension: CGFloat) -> CGFloat {
-    return (sideDimension - smallestScreenDimension()) / 2
+    let playAgain = PlayAgain(frame: collectionView.frame)
+
+    footer.addSubview(playAgain)
+
+    playAgain.autoresizingMask = UIViewAutoresizing.FlexibleLeftMargin | UIViewAutoresizing.FlexibleRightMargin
+
+    return footer
   }
 
   private func cellLength() -> CGFloat {
--- MarkersOnGrid/TicTacToe/PlayAgain.swift
+++ MarkersOnGrid/TicTacToe/PlayAgain.swift
@@ -1,4 +1,20 @@
 import UIKit
 
 class PlayAgain: UIButton {
+
+  required init(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setTitle("Play Again", forState: UIControlState.Normal)
+    titleLabel!.font = UIFont(name: "Futura-Medium", size: 35.0)
+    setTitleColor(Colors.PRIMARY_TEXT, forState: UIControlState.Normal)
+    titleLabel!.textAlignment = .Center
+
+    sizeToFit()
+
+  }
+
 }
